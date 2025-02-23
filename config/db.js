e6457f3758e754895c42b03af96bcc2b427e7a28
unknown <Rahul>
2025-02-23 16:46:18 +0530

    Added flies

---
+++ config/db.js
@@ -0,0 +1,16 @@
+
+const mongoose = require("mongoose")
+require("dotenv").config()
+
+
+const connectDB = async() => {
+    try{
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log("MongoDB connection SUCCESS")
+    } catch(error){
+        console.log("MONGODB Connection Failed",error)
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
